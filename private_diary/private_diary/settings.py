ac7b7b182c9c1124e1a3f008c725d3b529f521f7
kichinosukey <kichinosuke.fukuhara@gmail.com>
2020-06-01 16:21:03 +0900

    [update] static files dirs

--- private_diary/private_diary/settings.py
+++ private_diary/private_diary/settings.py
@@ -117,12 +117,18 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+##########
+# Static #
+##########
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = {
+    os.path.join(BASE_DIR, 'static'),
+}
 
 ###########
 # Logging #
